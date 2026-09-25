3b1d0f723d905674fe9a28257334e55d0af854ef
Kevin Espinoza <kvnjet@gmail.com>
2026-09-24 22:14:29 -0600

    Revert "Prueba de distinto manera de subir los cambios"
    This reverts commit 8d0b164faaabcd116bfcfec0db3ee150850f6162.

--- DnsExistsChecker.java
+++
@@ -1,9 +0,0 @@
-package cr.ac.tec.ic7602.dnsapi.dns;
-
-/**
- * Verifica si un dominio tiene un registro almacenado.
- * Permite cambiar de proveedor (Supabase/Firebase) sin tocar los handlers.
- */
-public interface DnsExistsChecker {
-    boolean exists(String domain);
-}
